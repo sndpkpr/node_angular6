cae27a5c4ea46f34942b6878412348791784b027
sndpkpr <sndpkpr33@gmail.com>
2018-09-24 00:21:29 +0530

    structure angular

---
+++ hycarebridapi/README.md
@@ -0,0 +1,1 @@
+TO lazy to write today.
--- hycarebridwebfront/src/app/app.component.ts
+++ hycarebridwebfront/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hycarebridwebfront';
+export class AppComponent implements OnInit {
+  title = 'Hycarebrid';
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
 }
