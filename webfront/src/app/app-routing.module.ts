5946b264eacf172ba4e48a823541dc02dcf77047
sndpkpr <sndpkpr33@gmail.com>
2018-10-06 18:17:52 +0530

    login component - need changes

--- webfront/src/app/app-routing.module.ts
+++ webfront/src/app/app-routing.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './library/shared/components/home/home.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: './library/shared/auth/auth.module#AuthModule',
+        // canActivate: [AnonymousGuard]
+      },
+      {
+        path: 'signup',
+        loadChildren: './library/shared/auth/auth.module#AuthModule',
+        // canActivate: [AnonymousGuard]
+      },
+      // {
+      //   path: 'news',
+      //   loadChildren: './areas/global-pages/open-article/open-article.module#OpenArticleModule'
+      // },
+      // {
+      //   path: 'resetpassword/:id',
+      //   loadChildren: './areas/global-pages/reset-password/reset-password.module#ResetPasswordModule'
+      // }
+    ]
+  },
 ];
 @NgModule({
   imports: [
@@ -11,4 +36,4 @@ const routes: Routes = [
   ],
   exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
