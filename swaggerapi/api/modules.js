8ee43f551f796d6c57165e015714aac2562da477
sndpkpr <sndpkpr33@gmail.com>
2018-10-09 17:14:07 +0530

    common include helper files

--- swaggerapi/api/modules.js
+++ swaggerapi/api/modules.js
@@ -1,8 +1,17 @@
-var normalizedPath = require("path").join(__dirname, "models");
+const modelPath = require("path").join(__dirname, "models");
 
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
+const helperPath = require("path").join(__dirname, "helpers");
+
+require("fs").readdirSync(modelPath).forEach(function(file) {
   if (file.match(/\.js$/) !== null) {
     var name = file.replace('.js', '');
     module.exports[name] = require('./models/' + file);
+  }
+});
+
+require("fs").readdirSync(helperPath).forEach(function(file) {
+  if (file.match(/\.js$/) !== null) {
+    var name = file.replace('.js', '');
+    module.exports[name] = require('./helpers/' + file);
   }
 });
